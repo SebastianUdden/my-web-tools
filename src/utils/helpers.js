528063a12081ecf2661ad34c29cfd05f50ddcc42
SEBSOUNdS <sebastian.udden@gmail.com>
2019-07-03 13:44:45 +0200

    Added save backup of rememory to client and formatted naming

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -39,7 +39,10 @@ export const downloadObjectAsJson = (exportObj, exportName) => {
     encodeURIComponent(JSON.stringify(exportObj));
   let downloadAnchorNode = document.createElement('a');
   downloadAnchorNode.setAttribute('href', dataStr);
-  downloadAnchorNode.setAttribute('download', exportName + '.json');
+  downloadAnchorNode.setAttribute(
+    'download',
+    `${exportName}-${new Date().toLocaleString().replace(', ', '-')}.json`
+  );
   document.body.appendChild(downloadAnchorNode); // required for firefox
   downloadAnchorNode.click();
   downloadAnchorNode.remove();
